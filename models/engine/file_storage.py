0852171c2fcd76e164dbf84c60a5bd32ba65ad3b
Trefania <107484364+Trefania@users.noreply.github.com>
2023-02-14 19:55:04 +0200

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,19 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """A method to retrieve one object """
+        if cls in classes.values() and id and type(id) == str:
+            d_obj = self.all(cls)
+            for k, v in d_obj.items():
+                if k.split(".")[1] == id:
+                    return v
+        return None
+
+    def count(self, cls=None):
+        """ A method to count the number of objects in storage"""
+        info = self.all(cls)
+        if cls in classes.values():
+            info = self.all(cls)
+        return len(info)
