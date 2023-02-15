e8af6b528d8318bb13f3fec34827869a96b80104
Trefania <107484364+Trefania@users.noreply.github.com>
2023-02-15 21:08:30 +0200

    State objects that handles all default RESTFul

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""API Blueprint Views Module
+"""creates app_views which is an instance of  API Blueprint Views Module
 """
 from flask import Blueprint
 
 app_views = Blueprint('app_views', '__name__', url_prefix='/api/v1')
+
 from api.v1.views.index import *
+from api.v1.views.states import *
