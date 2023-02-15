99822ee44e44984bda8c2e59977b3f79f7fe23ab
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-02-15 07:24:58 +0100

    unittests

--- models/base_model.py
+++ models/base_model.py
@@ -28,6 +28,9 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -42,10 +45,6 @@ class BaseModel:
                 self.updated_at = datetime.utcnow()
             if kwargs.get("id", None) is None:
                 self.id = str(uuid.uuid4())
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = self.created_at
 
     def __str__(self):
         """String representation of the BaseModel class"""
