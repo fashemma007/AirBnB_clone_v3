dec50227b37ce99d5a2d2ffbb53fd0d7fe17ed52
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-02-17 08:51:29 +0100

    feat[amenities]: delete an amenity whose id is given

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -24,6 +24,17 @@ def get_amenities(amenity_id=None):
     return jsonify(amenity.to_dict())
 
 
+@app_views.route("amenities/<amenity_id>", methods=["DELETE"],
+                 strict_slashes=False)
+def delete_amenity(amenity_id):
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    storage.delete(amenity)
+    storage.save()
+    return jsonify({}), 200
+
+
 @app_views.route("amenities", methods=["POST"], strict_slashes=False)
 @app_views.route("amenities/<amenity_id>", methods=["PUT"], strict_slashes=False)
 def create_amenities(amenity_id=None):
