d5ab09a3f11108a60e0f9c285216437e838be96c
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-02-18 18:25:12 +0100

    feat[hash]: secure passwords with hashing mechanism

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,7 @@ import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
+from hashlib import md5
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -48,8 +49,9 @@ class BaseModel:
 
     def __str__(self):
         """String representation of the BaseModel class"""
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-                                         self.__dict__)
+        return "[{:s}] ({:s}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__
+        )
 
     def save(self):
         """updates the attribute 'updated_at' with the current datetime"""
@@ -67,6 +69,10 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if "password" in new_dict:
+            pwrd = new_dict["password"]
+            new_dict["password"] = md5(
+                pwrd.lower().encode('utf-8')).hexdigest()
         return new_dict
 
     def delete(self):
