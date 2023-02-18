ef5366170c1b9299f9a36a1e50a243c32f62be2b
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-02-18 18:26:49 +0100

    feat[helpers]: files for testing and debuggind

--- test_get_count.py
+++ test_get_count.py
@@ -1,11 +1,69 @@
 #!/usr/bin/python3
-""" Test .get() and .count() methods
+"""Testing file
 """
-from models import storage
-from models.state import State
+import json
+import requests
 
-print("All objects: {}".format(storage.count()))
-print("State objects: {}".format(storage.count(State)))
+if __name__ == "__main__":
+    """ get the state with cities
+    """
+    r = requests.get("http://0.0.0.0:5000/api/v1/states")
+    r_j = r.json()
 
-first_state_id = list(storage.all(State).values())[0].id
-print("First state: {}".format(storage.get(State, first_state_id)))
+    state_id = None
+    for state_j in r_j:
+        rs = requests.get(
+            "http://0.0.0.0:5000/api/v1/states/{}/cities".format(state_j.get('id')))
+        rs_j = rs.json()
+
+        if len(rs_j) != 0:
+            state_id = state_j.get('id')
+            break
+
+    if state_id is None:
+        print("State with cities not found")
+
+    """ get city
+    """
+    r = requests.get(
+        "http://0.0.0.0:5000/api/v1/states/{}/cities".format(state_id))
+    r_j = r.json()
+
+    city_id = None
+    for city_j in r_j:
+        rc = requests.get(
+            "http://0.0.0.0:5000/api/v1/cities/{}/places".format(city_j.get('id')))
+        rc_j = rc.json()
+
+        if len(rc_j) != 0:
+            city_id = city_j.get('id')
+            break
+
+    if city_id is None:
+        print("City without places not found")
+
+    """ get place 
+    """
+    r = requests.get(
+        "http://0.0.0.0:5000/api/v1/cities/{}/places".format(city_id))
+    r_j = r.json()
+    place_id = r_j[1].get('id')
+
+    """ Get user
+    """
+    r = requests.get("http://0.0.0.0:5000/api/v1/users")
+    r_j = r.json()
+
+    user_id = r_j[0].get('id')
+    """ POST /api/v1/places/<place_id>/reviews
+    """
+    print(place_id)
+    r = requests.post("http://0.0.0.0:5000/api/v1/places/{}/reviews/".format(place_id), data=json.dumps(
+        {'user_id': user_id, "name": "Amazing Experience", 'text': "NewReview"}), headers={'Content-Type': "application/json"})
+
+    print(r.json)
+    print(r.status_code)
+    r_j = r.json()
+    print(r_j.get('id') is None)
+    print(r_j.get('user_id') == user_id)
+    print(r_j.get('text') == "NewReview")
