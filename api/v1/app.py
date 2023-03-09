c7afd365c4471c11186c5e8edf79dd805ad2dc9c
fasogbaemmanuel <fasogbaemmanuel@gmail.com>
2023-03-09 21:46:10 +0000

    change app port

--- api/v1/app.py
+++ api/v1/app.py
@@ -28,5 +28,5 @@ def not_found(error):
 
 if __name__ == "__main__":
     HOST = getenv('HBNB_API_HOST', default='0.0.0.0')
-    PORT = getenv('HBNB_API_PORT', default=5000)
+    PORT = getenv('HBNB_API_PORT', default=5002)
     app.run(host=HOST, port=PORT, threaded=True, debug=True)
